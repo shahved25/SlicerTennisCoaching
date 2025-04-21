ac62417cf904efd25e9a0820a4bd17db767b698c
Ved Shah <167489847+shahved25@users.noreply.github.com>
2025-04-21 11:59:41 -0400

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -1,11 +1,9 @@
-from analyze_video import analyze_video
 import cv2
 import numpy as np
 import requests
 import tempfile
-import os
 from cvzone.PoseModule import PoseDetector
-import concurrent.futures
+import os
 
 
 def process_frame(frame, detector):
@@ -14,7 +12,11 @@ def process_frame(frame, detector):
 
     # Check for person, add lm locations to frame list
     if bboxInfo:
+        # for i in lmList:
+        #     i.insert(0, lmList.index(i))
+
         return (lmList, frame)
+
     else:
         return None
 
@@ -55,18 +57,17 @@ def handle_aws_file(video_url):
     cap.release()
     os.remove(tmp_file_path)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Process each frame in parallel
-        futures = [executor.submit(process_frame, frame, detector) for frame in frames]
+    # Process frames sequentially instead of in parallel
+    for frame in frames:
+        result = process_frame(frame, detector)
+        if result:
+            frameList.append(result)
 
-        for future in concurrent.futures.as_completed(futures):
-            frameList.append(future.result())
-
-    print(frameList)
+    # print(len(frameList))
     # frames = analyze_video(frameList, type='fh', number_of_shots=1)
-    # print(frames)
+    #print(frames)
     return frameList
-#
+
 # # Example usage
-# video_url = 'https://user-temporary-uploads.s3.eu-central-1.amazonaws.com/01J52DY8KG1GDNVD7DMZ031S73?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA5B2PC3KUVAXJDCRQ%2F20240812%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20240812T043559Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEI3%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDGV1LWNlbnRyYWwtMSJIMEYCIQCiF162NJoi7P4gc0quNxjEJye%2B3aLYHLdJ4sOFgTw9agIhAIWvmwfRl3nhWaMo8JR6l94QIIScUPv3LeiMFhFDshhzKpsDCIb%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQBBoMODk3Mjc3MTU2MDA5IgwnqyCyG%2Beb3FhRQvQq7wI78ys3oJGvGoKtKer0hlqV%2F4VCfxTQeIe4APU3AtipwwOn0PplmcMA2eUSg8L3uXIynsyH3ZQpl2p%2FJHonBISRyzUKKPhZSpEv%2BoSWED1H2O25igB64xWVfhUUY%2BZUmyXpUpLahGN7gobpBcwhtU0zPoLG5fLlm0V1TnSfeMG64e42qgOoCvEqZhBUruX%2FSaSxjq3Hqw2NWaAOle%2BlIvVYCQfnB0oczgUif3yeWUEeCctZhT2aGPC9I6EnnPyuelVTRoEUxN%2FY8hwOlLfaOHFv1muN9jFxudEu2mMpgXnz6mmVVr13ho%2FcoEalArMRBONz6TfhcUD1sXQ8y3eDMrHJkMXlc7%2BtKG8YZEsNVN6G8CKFpS9krIYTs2RWQzgo3BintgecdV1STv6DEaMVjiiZ5qA3JXWmqLZfP%2BKa%2BqEs1BjKZzT%2Bf80Yd9ZG0n5P7BUsoTiK%2BTFOb4GjRNVU45MXM06EOEKkjxn2W%2FWp7bDaMK6i5rUGOpwBmIYmvqx8GPus3mSxaBK5M81KfiGFy7SpR2d4JRIkfTz%2BDwV%2BVEHmR%2Fbszz2UD50YfxFOywAMth4qnogMGfnZrZux6xduaK%2Fq2RJ2F3qXnNzhq0aPlHeHQ0DBRRh8f1z7Tu1AMTygZdmDlb3t1X1TKCMhdhzeRi1ivWI1HjMeSaS9YNt7IEzGz6%2FmbBf%2FU2VtYVBmguFQAZqUuJEk&X-Amz-Signature=8fa0362bf4ae944b14712fdf7a792ea24e069d89ddb92dd0b7887add2f8ad80f&X-Amz-SignedHeaders=host&x-id=GetObject'
+# video_url = 'https://user-temporary-uploads.s3.eu-central-1.amazonaws.com/01J7VRJQ255KHMMXN9CZH9FV5D?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA5B2PC3KUVMB5K7SM%2F20240915%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20240915T211420Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEM3%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDGV1LWNlbnRyYWwtMSJIMEYCIQDvCrHNk%2FEkf7Rzb2PjrawG6Hvqtdct%2BOUbZGlBy91c5QIhAPJwYaDb5rA6gbSFr1K03mJzjHIA%2B5dJeM0R%2FlX%2BniyyKpsDCPb%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQBBoMODk3Mjc3MTU2MDA5IgxQ6S1rs8wUdiMS1Ggq7wK8jOqu4wGliJSyMLhZcM7DJe4GTNES6t8Ve1B%2FblassQLRgnCOCAmzJ%2FNzBZz6WOBYdz3OjI0Ial0EsKLqf%2F9oCwDBWx2GTuklvf9PQ21Tih1v6%2FznK8WHYqQwK994k1yZKWRZnkqtCfbmnCncE86qazf87cuXv6neKGqhS5Id4o%2FNSE%2F31aa8j6lClwpJwdzwLoJQ5luSXt0OCyOFsctT03k9y9AVDGa8OqGJbZQtdi85R%2FlfmSzsOv7gsrZTIDZ4TVQW%2BucWsIyGP67hbjzE2bBfY3XZHCeyQPM5Sk3%2BoTbnmVJP4MDnTXe0MoJJgZuVtfWHOY56oEEhKKCB41oZ6e264G8KC8R0czNe%2B8Jyg9w%2F3aD64MY5mcJ0KerPsSJaGKWoGvx8ujISBt1NmCePLk5yHkku56SjTwuYfC%2BhTDKwjWuFFmlChQvOHTsJdEKYT6tJP%2B8w7LD4Muxm7RRlR9Fw%2FRsmNs8hdcfA5BIQMOiXnbcGOpwBJMqauEtmIJqvJgKM%2B6wAFmjA8dWEzaof4geo3FzlNTT4pjDG5Bd5hSMgGRDEBh613c3FpyskdlVLlOMTjxmDuVyXnTczQz5%2FE54pJueDerZnfdSRP8cURCrU%2FlqWQKAXoi%2Fj1BDZEmBRkFCMSSDn32tfcgdpHsfeJOzkDvzroa4vTX%2F03Rn2aeWI3WuEHf5lEmEE8yPoPN%2BVCKJZ&X-Amz-Signature=8786a7c3287c3afa4c105147575881d4bac79475203af44a7a477b97d46b6cce&X-Amz-SignedHeaders=host&x-id=GetObject'
 # handle_aws_file(video_url)
